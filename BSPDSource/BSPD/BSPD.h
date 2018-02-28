e42fc722154834448627ebc09a3597890d2876e8
fgodt <fgodtdev@hotmail.com>
2018-02-27 22:38:10 -0800

    add lose file

---
+++ BSPDSource/BSPD/BSPD.h
@@ -0,0 +1,181 @@
+#ifndef __BSPD_H__
+#define __BSPD_H__
+
+#if _WIN32||_WIN64
+#define _DLLEXPORT _declspec(dllexport)
+#else
+#define _DLLEXPORT 
+#endif
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+    
+#include "bspd_coder.h"
+#if __ANDROID_NDK__
+#include <libavcodec/jni.h>
+#endif
+
+#include <time.h>
+#ifdef __cplusplus
+}
+#endif
+
+
+typedef struct {
+	int num;
+	int x;
+	int y;
+	char * path;
+}MCtx;
+
+
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT void BSPDTest();
+
+
+/**
+ * 创建解码实例
+ **/
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT BSPDContext* BSPDCreateCtx();
+
+/**
+ * 打开流
+ * @param ctx the bsp ctx
+ * @param input the media path can be http url rtmp url local path
+ * @param option the bsp option 
+ *          -d for debug
+ *          -ha decode with audio
+ *          -ch audio channels
+ *          -sr audio sample rate
+ **/
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDOpen(BSPDContext *ctx ,char *input, char *options);
+
+
+/**
+ * 获取解码后的YUV数据
+ * 数据格式为YUV420p
+ * @param bspdctx the bsp ctx
+ * @param ydata get y data
+ * @param udata get u data
+ * @param vdata get v data
+ **/
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDGetYUV(BSPDContext *bspdctx,char *ydata,char *udata,char *vdata);
+
+
+/**
+ * 获取解码后的YUV数据和时间
+ * 数据格式为YUV420p
+ * @param bspdctx the bsp ctx
+ * @param ydata get y data
+ * @param udata get u data
+ * @param vdata get v data
+ * @param vpts video pts
+ * @param vduration video duration
+ **/
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDGetYUVWithTime(BSPDContext *bspdctx, char *ydata, char *udata, char *vdata, int64_t *vpts, int64_t *apts, int64_t *vduration, int64_t *aduration);
+
+attribute_deprecated
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDGetPCM(BSPDContext *bspdctx, char *rawdata);
+
+/**
+* 获取解码后的数据和时间
+* video数据格式为YUV420p
+* audio数据格式为pcm float32
+* @param bspdctx the bsp ctx
+* @param ydata 
+*               if return is video ydata is y data
+*               if return is audio ydata is pcm data
+* @param udata
+*               if return is vidoe udata is u data
+*               if return is audio udata first 4 byte is audio pcm data size
+* @param vdata get v data
+* @param pts  is audio/video pts
+* @param duration is audio/video duration
+* return  1 for video data 
+*         2 for audio data 
+*         other for error 
+**/
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDGetRawDataWithTime(BSPDContext *bspdctx, char *ydata, char *udata, char *vdata, int64_t *pts, int64_t *duration);
+
+attribute_deprecated
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT BSPDPacketData* BSPDCreatePacket(BSPDContext *bspdctx,int *opcode);
+
+attribute_deprecated
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDGetPacket(BSPDContext *bspdctx,BSPDPacketData *pkt);
+
+attribute_deprecated
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDDecodePacketV(BSPDContext *bspdctx,BSPDPacketData *pkt);
+
+attribute_deprecated
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDDecodePacketA(BSPDContext *bspdctx,BSPDPacketData *pkt);
+
+attribute_deprecated
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDFreePacket(BSPDContext *bspdctx,BSPDPacketData *pkt);
+
+/**
+ * 关闭解码器
+ * 非线程安全的方法 使用前应该调用 BSPDAbort(BSPDContext *bspdctx)
+ * @param bspdctx is ctx
+ **/
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDClose(BSPDContext *bspdctx);
+
+/**
+ * 设置Log回调 通过回调可以自定义LOG输出
+ * @param bspdctx bsp ctx
+ * @param call callback func
+ **/
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDSetLogCallback(BSPDContext *bspdctx,BSPDLogCallback call);
+
+/**
+ * 终止正在进行的解码操作
+ * 有些操作会比较耗时 使用此方法可以快速终止操作
+ * @param bspdctx is bsp ctx
+ **/
+#ifdef __cplusplus
+extern "C"
+#endif
+_DLLEXPORT int BSPDAbort(BSPDContext *bspdctx);
+
+#endif // ! __BSPD_H__
---
+++ BSPDSource/BSPD/bspd_coder.h
@@ -0,0 +1,197 @@
+﻿#ifndef __BSPD_CODER_H__
+#define __BSPD_CODER_H__
+
+#if _WIN32|_WIN64
+#define _CRT_SECURE_NO_WARNINGS 1
+#endif
+
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/imgutils.h>
+#include <libswscale/swscale.h>
+#include <libswresample/swresample.h>
+//#include "bspd_mutex.h"
+//#include "bspd_cond.h"
+//#include "FDCore.h"
+
+
+/**
+ * BSPD OPTIONCODE define
+ */
+#define BSPD_OP_OK				0
+#define BSPD_OPEN_ERRO			1
+#define BSPD_FIND_INFO_ERRO		2
+#define BSPD_NO_VIDEOS_FOUND	4
+#define BSPD_NO_CODEC_FOUND		8
+#define BSPD_OPEN_CODEC_ERRO	16
+#define BSPD_NO_MEMY			0x20
+#define BSPD_INPUT_ERROR		0x40	//bspdopen input 参数错误
+#define BSPD_OPTIONS_ERROR		0x80	//bspdopen options 参数错误
+#define BSPD_USE_NULL_ERROR		0x100
+#define BSPD_AVLIB_ERROR		0x200
+#define BSPD_TIMEOUT            0x400
+#define BSPD_ERRO_UNDEFINE		0x7fffffff //[0111 ... 1111]
+
+/**
+ * LOG LEVEL DEFINE
+ */
+#define BSPD_LOG_ERROR			1
+#define BSPD_LOG_DEBUG			255
+
+
+#define BSPD_FREE_MARK 0xf0f1f2
+#define BSPD_CLOSE_MARK 0x2f1f0f
+
+#define BSPDISNULL(x) ((x) == NULL || (x)->closeMark != BSPD_CLOSE_MARK || (x)->freeMark != BSPD_FREE_MARK)
+#define BSPPKTISNULL(x) ((x) == NULL || (x)->pkt == NULL)
+
+#define MAX_MEDIATYPE_INDEX 10
+
+static int av_set_java_vm_flags;
+
+ //int bspd_hb_abort;
+
+typedef void( *BSPDLogCallback)(char *log);
+
+typedef	struct {
+    AVFormatContext *pFormatCtx;
+    AVCodecContext	*pCodecCtx;
+    AVCodec			*pCodec;
+    AVCodecContext  *pACodecCtx;
+    AVCodec         *pACodec;
+    AVFrame			*pFrame;
+    AVFrame			*pFrameYUV;
+    AVFrame         *pPcmFrame;
+    struct SwsContext *imgSwsCtx;
+    struct SwrContext *pcmSwrCtx;
+    AVPacket		*packet;
+    AVDictionary	*optDic;
+    unsigned char	*pBuf; //temp buf use to rw frame data
+    int             pSize;
+    int				LOGLEVEL;
+    int				fVIndex;//first video stream index
+    int             fAIndex;//first audio stream index
+    int				initDone;
+    int             allMediaTypeIndex[MAX_MEDIATYPE_INDEX];
+    int64_t         start_clock;
+    int64_t         timeout;
+    char            istimeout;
+
+    //HW 
+    AVBufferRef     *hwBufCtx;
+    enum AVPixelFormat hwPixFmt;
+    int             useHW;
+    int             hwInitDone;
+    AVFrame         *phwImgFrame;
+
+    //audio
+    int             hasAudio;
+    int             channles;
+    int             sampleRate;
+//    FDCCtx          *fdcCtx;
+ //   FDCCtx          *fdSCtx;
+}BSPDCoder;
+
+typedef struct {
+    AVFrame         *pYuvData;
+    int             size;
+    int             width;
+    int             height;
+    int             cwidth;
+    int             cheight;
+    double          pts;
+}BSPDFrameData;
+
+#define QUEUE_SIZE 10
+typedef struct BSPDFrameQueue {
+    BSPDFrameData   queue[QUEUE_SIZE];
+    int             max_size;
+    int             size;
+    int             rindex;
+    int             windex;
+    int             rindex_show;
+   // BSPDMutex       *mutex;
+  //  BSPDCond        *cond;
+}BSPDFrameQueue;
+
+typedef struct BSPDPacketData {
+    AVPacket            *pkt;
+    enum AVMediaType    pktType;
+    int                 closeMark;
+    int                 freeMark;
+}BSPDPacketData;
+
+/**
+* BSPD context
+* 用于各个解码函数之间数据传递
+*/
+typedef	struct {
+
+    BSPDCoder		*pCoder;
+
+    //输入媒体url
+    char			*inputPath;
+    //打开输入的 options
+    char			*options;
+    //解码宽度
+    int				codedWidth;
+    //解码高度
+    int				codedHeight;
+    //视频宽度
+    int				width;
+    //视频高度
+    int				height;
+    //YUV中Ydata size
+    int				ysize;
+    //操作返回code
+    int				opcode;
+    //frame 时间戳
+    int64_t			timeStamp;
+    //video fps
+    int             vFps;
+    //video duration
+    int64_t         vDuration;
+
+    BSPDLogCallback	logCallback;
+
+    int             closeMark;
+    int             freeMark;
+    int             hb;
+    int             bspd_hb_abort;
+}BSPDContext;
+
+
+int bc_parse_options(BSPDContext *ctx);
+
+int bc_set_default_options(BSPDContext *ctx);
+
+int bc_init_coder(BSPDContext *ctx);
+
+int bc_get_yuv(BSPDContext *ctx);
+
+int bc_get_raw(BSPDContext *ctx);
+
+int bc_close(BSPDContext *ctx);
+
+int bc_test();
+
+/**
+ * bc log
+ * 使用bc log 来输出log 日志
+ *
+ * @param ctx BSPDContext
+ * @param LEVEL log 输出等级
+ * @param fmt 日志输出格式
+ * @param ... 参数
+ */
+int bc_log(BSPDContext *ctx,int LEVEL,const char * fmt, ...);
+
+int bc_get_packet(BSPDContext *ctx, BSPDPacketData *pkt);
+
+int bc_decode_audio_packet(BSPDContext *ctx,BSPDPacketData *p);
+
+int bc_decode_video_packet(BSPDContext *ctx,BSPDPacketData *p);
+
+int bc_free_packet();
+
+#endif // !__BSPD_CODER_H_
